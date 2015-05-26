24aa83e30081f7e4abfc03a3dc92d4975ca2df98
Ryan Daniels <ryan.daniels@gmail.com>
2015-05-25 17:00:43 -0700

    SymbolTable now added automatically in CallEnv

--- cacti/main.py
+++ cacti/main.py
@@ -5,8 +5,6 @@ from cacti.lang import Function, Method, MethodDefinition
 def set_up_main_call_env():
     mainobj = make_main()
     call_env = CallEnv(mainobj, mainobj.name)
-    table = SymbolTable()
-    call_env.symbol_stack.push(table)
     push_call_env(call_env)
 
 def main():
